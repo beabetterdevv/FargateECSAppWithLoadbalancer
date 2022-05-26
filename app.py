0f3c9c3b0667facb690ac34514e672913b8d3b56
Dhrithi Chidananda <dchidana@asu.edu>
2022-05-26 12:36:49 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,15 +5,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    time.sleep(180)
+    #time.sleep(180)
     return render_template('index.html')
 
 @app.route('/app')
 def blog():
-    time.sleep(180)
+    #time.sleep(180)
     return "Hello, from App!"
 
 
 
 if __name__ == '__main__':
-    app.run(threaded=True,host='0.0.0.0',port=8081)
+    app.run(threaded=True,host='0.0.0.0',port=8080)
